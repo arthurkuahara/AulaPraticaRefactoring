516aafe79ca600a010a6d7ee58155e15ee97f532
Arthur Veloso <56210323+arthurkuahara@users.noreply.github.com>
2021-03-08 02:09:56 -0300

    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -7,4 +7,9 @@ public class NewReleasePrice extends Price {
     return daysRented * 3;
   }
 
+      public int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2 : 1;
+    }
+
+
 }
