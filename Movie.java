14630efff2daea71b0e7860871f620102dd859e6
Arthur Veloso <56210323+arthurkuahara@users.noreply.github.com>
2021-03-08 02:05:59 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -5,19 +5,32 @@ public class Movie {
   public static final int  NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
   public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
+    _title = title;
+    setPriceCode(priceCode);
   }
 
   public int getPriceCode() {
-      return _priceCode;
+    return _price.getPriceCode();
   }
 
   public void setPriceCode(int arg) {
-      _priceCode = arg;
+    
+    switch (arg) {
+      case REGULAR:
+        _price = new RegularPrice();
+        break;
+      case CHILDRENS:
+        _price = new ChildrensPrice();
+        break;
+      case NEW_RELEASE:
+        _price = new NewReleasePrice();
+        break;
+      default:
+        throw new IllegalArgumentException("Incorrect Price Code");
+    }
   }
 
   public String getTitle (){
@@ -43,6 +56,13 @@ public class Movie {
     }
     return thisAmount;
   }
+
+    public int getFrequentRenterPoints(int daysRented) {
+    if ((getPriceCode() == Movie.NEW_RELEASE) &&
+            daysRented > 1) return 2;
+    return 1;
+  }
+
 
 
 }
